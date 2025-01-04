a6d47d42cedee938f5e32853a60e49aff08eaadc
Arunjoshua1410 <arunjoshua2001@gmail.com>
2025-01-04 22:27:58 +0530

    first commit

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,44 +1,112 @@
-import { Link } from "react-router-dom"
+ import { Link } from "react-router-dom"
 
+
+// export const Projects = () => {
+//   return (
+//       <>
+//         <div className="project row">
+//              <div className="headpro col-12">
+//                  <h1 className="d-flex justify-content-center">My Projects</h1>
+                 
+//              </div>
+//              <div className="frame col-4 d-flex justify-content-center">
+//                   <iframe src="https://tamilnaduebbillcalculatorhome.netlify.app/" frameborder="0" width={"800px"} height={"500px"}></iframe>
+//             </div>
+//             <div className="ebbill col-4  d-flex justify-content-center">
+//                   <iframe src="https://bodymassindexproject.netlify.app/" frameborder="0" width={"800px"} height={"500px"}></iframe>
+                 
+//             </div>
+//             <div className="ebbillbt col-4">
+//                   <iframe src="https://spicyhuttwebsite.netlify.app/" frameborder="0" width={"800px"} height={"500px"}></iframe>
+//             </div>
+//             <div className="bmi col-4">
+//                   <h6>To see the Full View Of the site Click the Button</h6>
+//                   <button ><a href="https://tamilnaduebbillcalculatorhome.netlify.app/" target="_blank" rel="noreferrer"  className="btn-link">EB Bill project</a></button>
+//              </div>
+//              <div className="bmi col-4">
+//                   <h6>To see the Full View Of the site Click the Button</h6>
+//                   <button ><a href="https://bodymassindexproject.netlify.app" target="_blank" rel="noreferrer"  className="btn-link">Body Mass Index project</a></button>
+//              </div>
+//              <div className="bmi col-4">
+//                   <h6>To see the Full View Of the site Click the Button</h6>
+//                   <button ><a href="https://spicyhuttwebsite.netlify.app/" target="_blank" rel="noreferrer" className="btn-link">Commercial project</a></button>
+//              </div>
+//              <div className="exit  col-12">
+//                    <Link to="/">
+//                       <button>Home Page</button>
+//                    </Link>
+                  
+//               </div>
+//         </div>
+       
+//       </>
+//   )
+// }
 
 export const Projects = () => {
-  return (
-      <>
-        <div className="project row">
-             <div className="headpro col-12">
-                 <h1 className="d-flex justify-content-center">My Projects</h1>
-                 
-             </div>
-             <div className="frame col-4 d-flex justify-content-center">
-                  <iframe src="https://tamilnaduebbillcalculatorhome.netlify.app/" frameborder="0" width={"800px"} height={"500px"}></iframe>
+      return (
+        <>
+          <div className="project row">
+            <div className="headpro col-12">
+              <h1 className="d-flex justify-content-center">My Projects</h1>
             </div>
-            <div className="ebbill col-4  d-flex justify-content-center">
-                  <iframe src="https://bodymassindexproject.netlify.app/" frameborder="0" width={"800px"} height={"500px"}></iframe>
-                 
+            <div className="frame col-4 d-flex justify-content-center">
+              <iframe
+                src="https://tamilnaduebbillcalculatorhome.netlify.app/"
+                title="Tamil Nadu EB Bill Calculator Project"
+                frameBorder="0"
+                width="800px"
+                height="500px"
+              ></iframe>
+            </div>
+            <div className="ebbill col-4 d-flex justify-content-center">
+              <iframe
+                src="https://bodymassindexproject.netlify.app/"
+                title="Body Mass Index Project"
+                frameBorder="0"
+                width="800px"
+                height="500px"
+              ></iframe>
             </div>
             <div className="ebbillbt col-4">
-                  <iframe src="https://spicyhuttwebsite.netlify.app/" frameborder="0" width={"800px"} height={"500px"}></iframe>
+              <iframe
+                src="https://spicyhuttwebsite.netlify.app/"
+                title="Spicy Hutt Website Project"
+                frameBorder="0"
+                width="800px"
+                height="500px"
+              ></iframe>
             </div>
             <div className="bmi col-4">
-                  <h6>To see the Full View Of the site Click the Button</h6>
-                  <button ><a href="https://tamilnaduebbillcalculatorhome.netlify.app/" target="_blank" rel="noreferrer"  className="btn-link">EB Bill project</a></button>
-             </div>
-             <div className="bmi col-4">
-                  <h6>To see the Full View Of the site Click the Button</h6>
-                  <button ><a href="https://bodymassindexproject.netlify.app" target="_blank" rel="noreferrer"  className="btn-link">Body Mass Index project</a></button>
-             </div>
-             <div className="bmi col-4">
-                  <h6>To see the Full View Of the site Click the Button</h6>
-                  <button ><a href="https://spicyhuttwebsite.netlify.app/" target="_blank" rel="noreferrer" className="btn-link">Commercial project</a></button>
-             </div>
-             <div className="exit  col-12">
-                   <Link to="/">
-                      <button>Home Page</button>
-                   </Link>
-                  
-              </div>
-        </div>
-       
-      </>
-  )
-}
+              <h6>To see the Full View Of the site Click the Button</h6>
+              <button>
+                <a href="https://tamilnaduebbillcalculatorhome.netlify.app/" target="_blank" rel="noopener noreferrer" className="btn-link">
+                  EB Bill project
+                </a>
+              </button>
+            </div>
+            <div className="bmi col-4">
+              <h6>To see the Full View Of the site Click the Button</h6>
+              <button>
+                <a href="https://bodymassindexproject.netlify.app" target="_blank" rel="noopener noreferrer" className="btn-link">
+                  Body Mass Index project
+                </a>
+              </button>
+            </div>
+            <div className="bmi col-4">
+              <h6>To see the Full View Of the site Click the Button</h6>
+              <button>
+                <a href="https://spicyhuttwebsite.netlify.app/" target="_blank" rel="noopener noreferrer" className="btn-link">
+                  Commercial project
+                </a>
+              </button>
+            </div>
+            <div className="exit col-12">
+              <Link to="/">
+                <button>Home Page</button>
+              </Link>
+            </div>
+          </div>
+        </>
+      );
+    };
